d7e02341112d97376f4e6070b07c93353b83ecfe
Fer1196 <rodifer1196@hotmail.com>
2024-11-02 17:18:26 -0500

    Read data from Vercel

--- src/context/filtersContext.tsx
+++ src/context/filtersContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Filter } from "../utils/interfaces/Filter.interface";
 import { products as initialProducts } from "../mocks/products.json";
 import { Product } from "../utils/interfaces/Product";
@@ -29,12 +29,34 @@ export function FiltersProvider({
     price: 0,
     searchWord: "",
   });
+
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://mongvercel-git-main-fer1196s-projects.vercel.app/products`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async (res) => await res.json())
+      .then((res) => {
+        const productsArray = res.products;
+
+        setProductList(productsArray);
+      })
+      .catch((error) => {
+        setProductList([]);
+        console.error("Error al enviar la solicitud:", error);
+      });
+  }, []);
+
   return (
     <FilterContext.Provider
       value={{
         filters,
         setFilters,
-        products: initialProducts,
+        products: productList,
       }}
     >
       {children}
